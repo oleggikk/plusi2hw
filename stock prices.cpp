a0ff3f89867ad675858beae9e3b76716f8dd8229
anapluslap <91057358+lapshinaaa@users.noreply.github.com>
2023-01-06 11:40:20 +0300

    add leetcode task | stock prices

---
+++ stock prices.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int main()
+{
+    //TASK 14
+
+    //creating a customized array of prices
+    int sizeOfArray;
+    std::cout<<"Enter the length of your array: "<<std::endl;
+    std::cin>>sizeOfArray;
+
+    int *arr = new int[sizeOfArray];  //allocating memory for an array
+
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        std::cout<<"Enter the price of the stock: ";
+
+        int element;
+        std::cin>>element;
+        arr[i] = element;   //filling the created array with stock prices
+    }
+
+    // printing out the initial array:
+    std::cout<<"The initial array of stock prices is: "<<std::endl;
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        std::cout<<arr[i]<<" ";
+    }
+
+    std::cout<<std::endl;
+
+    int maximum = 0;  //maximum profit
+
+    for(int i = 0; i < sizeOfArray - 1; i++)
+    {
+        for (int j = i + 1; j < sizeOfArray; j++)
+        {
+            if(arr[j] - arr[i] > maximum)
+            {
+                maximum = arr[j] - arr[i];
+            }
+        }
+    }
+
+    delete[] arr;
+
+    std::cout<<"The maximum profit you can achieve through this transaction is: "<< maximum;
+
+    return 0;
+}
